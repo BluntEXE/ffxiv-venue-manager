a6a6b646d3887a97c3d34383d6cbba4c6ff5b4b6
BluntEXE <rgcsubsonik@gmail.com>
2026-08-14 22:55:54 +0100

    chore: remove root app.config.js (EAS re-export for deleted apps/mobile)

--- app.config.js
+++
@@ -1,4 +0,0 @@
-// EAS Build reads config from the git root. Re-export the mobile app config
-// so prebuild finds android.package and projectId correctly.
-const { expo } = require('./apps/mobile/app.json');
-module.exports = { expo };
